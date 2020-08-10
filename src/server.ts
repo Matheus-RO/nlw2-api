206f9aa82903b1f18f204007fd28074477888216
matheus.oliveira <matheusoliveira.1641@gmail.com>
2020-08-10 20:08:32 -0300

    Correção da chamada do cors

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import routes from './routes';
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
 
 app.listen(8080, () => {
   console.log('Api Started');
